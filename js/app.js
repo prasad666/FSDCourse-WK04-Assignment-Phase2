998359c54f1918391fe4cf24e7a2c4772a6a824b
Prasad P N <prasad.p@dalason.com>
2022-04-19 23:32:41 +0530

    Added form validations

--- js/app.js
+++ js/app.js
@@ -25,6 +25,93 @@ window.onload = function() {
 
 
 function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function checkPasswordStrength() {
+
+}
+
+function submitHandler(e) {
+
+    e.preventDefault();
+    let email = document.getElementById('email');
+    let firstName =document.getElementById('firstName');
+    let lastName = document.getElementById('lastName');
+    let password1 = document.getElementById('password1');
+    let password2 = document.getElementById('password2');
+    let month = document.getElementById('month');
+    let year = document.getElementById('year');
+    let day = document.getElementById('day');
+
+    let chkMale = document.getElementById('male');
+    let chkFemale = document.getElementById('female');
+
+    if(firstName.value == "") {
+        alert("Please enter First Name!");
+        return false;
+    }
+
+    if(lastName.value == "") {
+        alert("Please enter Last Name!");
+        return false;
+    }
+
+    if(!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email.value)) {
+        alert('Email address is not valid or empty!');
+        return false;
+    }
+
+    if(password1.value == "") {
+        alert("Please enter password!");
+        return false;
+    }
+
+    if(password2.value == "") {
+        alert("Please enter confirm password!");
+        return false;
+    }
+
+    if(month.value == "") {
+        alert("Please choose Month!");
+        return false;
+    }
+
+    if(year.value == "") {
+        alert("Please choose Year!");
+        return false;
+    }
+
+    if(day.value == "") {
+        alert("Please choose Day!");
+        return false;
+    }
+
+    if(!chkMale.checked && !chkFemale.checked) {
+        alert('Please choose gender!');
+        return false;
+    }
+
+    
+
+    return false;
+}
+
+// Login page validation
+
+function validateLogin(e) {
+    e.preventDefault();
+    let email = document.getElementById('email');
+    let pass = document.getElementById('password');
+    if(!(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email.value))) {
+        alert('Email address is empty or not valid!');
+        return false;
+    }
+
+    if(pass.value == "") {
+        alert('Please enter password!');
+        return false;
+    }
+    return true;
 }
